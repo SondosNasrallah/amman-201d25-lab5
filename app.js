6d88ad43360df91d35ab15c3ee2bdd034256f2da
SondosNasrallah <sondos.nasrallah98@gmail.com>
2021-04-29 16:40:44 +0300

    done problem3

--- app.js
+++ app.js
@@ -51,11 +51,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sum1 = sum(a,b)[0];
+  let sum2 = sum(sum1, c)[0];
+  let multiply1 = multiply(a,b)[0];
+  let multiply2 = multiply(multiply1,c)[0];
 
+  return [sum2, multiply2 ,`${a} and ${b} and ${c} sum to ${sum2}.`,`The product of ${a} and ${b} and ${c} is ${multiply2}.`]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -70,15 +75,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+   let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+   function sumArray(sumArr) { //eslint-disable-line
+  
+    // let sA = sum(sumArr[0], sum(sumArr[1],sumArr[2])[0])[0];
+  
+    // return [sA, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sA} is their sum.`]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+//  testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
